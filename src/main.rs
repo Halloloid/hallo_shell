819e2214af7479cf7c5f16226804db80593b60f2
Halloloid <animation12321567@gmail.com>
2026-06-06 21:26:08 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).expect("Unabel to Read");
-
-    let command = command.trim_end();
-    println!("{command}: command not found");
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut command = String::new();
+        io::stdin().read_line(&mut command).expect("Unabel to Read");
+        println!("{}: command not found",command.trim());
+    }
 }
