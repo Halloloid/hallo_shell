f3ac0961772774e36a1e3909fd94ad54ad6bccf3
Halloloid <animation12321567@gmail.com>
2026-06-21 21:35:09 +0530

    Listing Multiple Jobs [skip ci]

--- src/commands/handel_jobs.rs
+++ src/commands/handel_jobs.rs
@@ -1,7 +1,15 @@
-pub fn run(back_jobs:&Vec<Option<(u8,u32,String)>>){
-    for i in back_jobs{
+pub fn run(back_jobs: &Vec<Option<(u8, u32, String)>>) {
+    let len = back_jobs.len() as u8;
+
+    for i in back_jobs {
         if let Some(job) = i {
-            println!("[{}]+  Running                 {}",job.0,job.2);
+            if len == job.0 {
+                println!("[{}]+  Running                 {}", job.0, job.2);
+            } else if len - 1 == job.0 {
+                println!("[{}]-  Running                 {}", job.0, job.2);
+            } else {
+                println!("[{}]   Running                 {}", job.0, job.2);
+            }
         }
     }
 }
