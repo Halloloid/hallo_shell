50322977e415fa75cdbafd21485284981f19cc7e
Halloloid <animation12321567@gmail.com>
2026-06-22 09:23:59 +0530

    Reaping Multiple jobs [skip ci]

--- src/commands/handel_jobs.rs
+++ src/commands/handel_jobs.rs
@@ -1,26 +1,25 @@
 use std::process::Child;
 
 pub fn run(back_jobs: &mut Vec<Option<(u8, Child, String)>>) {
-    let total_jobs = back_jobs.len() as u8;
+    let total_jobs = back_jobs.len();
     let mut index_to_remove = Vec::new();
 
-
-    for (index, job) in back_jobs.iter_mut().enumerate() {
-        if let Some(job) = job {
+    for (index, jobk) in back_jobs.iter_mut().enumerate() {
+        if let Some(job) = jobk {
+            let sign = if total_jobs-1 == index{
+                "+"
+            } else if total_jobs - 2 == index {
+                "-"
+            } else {
+                " "
+            };
+            
             match job.1.try_wait() {
                 Ok(None) => {
-                    let sign = if total_jobs == job.0 {
-                        "+"
-                    } else if total_jobs - 1 == job.0 {
-                        "-"
-                    } else {
-                        " "
-                    };
-
                     println!("[{}]{}  Running                 {}", job.0, sign, job.2);
                 }
                 Ok(Some(_status)) => {
-                    println!("[{}]+  Done                 {}",job.0,&job.2[..job.2.len()-2]);
+                    println!("[{}]{}  Done                 {}",job.0,sign,&job.2[..job.2.len()-2]);
 
                     index_to_remove.push(index);
                 }
@@ -29,7 +28,7 @@ pub fn run(back_jobs: &mut Vec<Option<(u8, Child, String)>>) {
         }
     }
 
-    for i in index_to_remove{
+    for i in index_to_remove.into_iter().rev(){
         back_jobs.remove(i);
     }
 }
