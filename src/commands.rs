86858e202270804395bba0551c24a6c56ba3330d
Halloloid <animation12321567@gmail.com>
2026-06-25 14:48:34 +0530

    Added history builtin [skip ci]

--- src/commands.rs
+++ src/commands.rs
@@ -6,4 +6,4 @@ pub mod handel_complete;
 pub mod handel_hallo;
 pub mod handel_jobs;
 
-pub const BUILT_IN:[&str;8] = ["type","echo","exit","pwd","cd","complete","hallo","jobs"];
+pub const BUILT_IN:[&str;9] = ["type","echo","exit","pwd","cd","complete","hallo","jobs","history"];
